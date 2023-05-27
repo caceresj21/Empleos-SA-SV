a3f23bf63fb8d3ee0420861749fb7356aee2948d
caceresj21 <caceresalbertogal10@gmail.com>
2023-05-27 05:52:05 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Empleos_SA_SV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
